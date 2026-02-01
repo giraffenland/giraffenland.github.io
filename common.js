5679a6527106eb826f2d8ccdb6239723030f468f
giraffenland <giraffenlanddc@gmail.com>
2026-02-02 00:46:09 +0100

    Add files via upload

--- common.js
+++ common.js
@@ -3,11 +3,11 @@ class CommonNavbar extends HTMLElement {
         this.innerHTML = `
         <nav class="navbar">
             <div class="nav-links">
-                <a href="index.html"><img src="icon.webp" alt="Logo"></a>
+                <a href="index.html"><img src="home.png" alt="Logo"></a>
                 <a href="https://discord.gg/Tkn83H6uQ5" target="_blank"><img src="dc.webp" alt="Discord"></a>
-                <a href="https://www.instagram.com/giraffenland.offical" target="_blank"><img src="insta.webp" alt="Instagram"></a>
-                <a href="https://www.tiktok.com/@giraffenteam" target="_blank"><img src="tiktok.webp" alt="TikTok"></a>
-                <a href="https://www.youtube.com/@giraffenlanddc" target="_blank"><img src="yt.webp" alt="YouTube"></a>
+                <a href="https://www.instagram.com/giraffenland.offical" target="_blank"><img src="insta.png" alt="Instagram"></a>
+                <a href="https://www.tiktok.com/@giraffenteam" target="_blank"><img src="tiktok.png" alt="TikTok"></a>
+                <a href="https://www.youtube.com/@giraffenlanddc" target="_blank"><img src="yt.png" alt="YouTube"></a>
                 <a href="about.html"><img src="about.webp" alt="Über Uns"></a>
             </div>
             <span class="navbar-text">© 2026 TeamGiraw | Mattis</span>
